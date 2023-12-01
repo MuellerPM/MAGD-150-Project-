00f5c70b3ed353b053c04429f49aad9b1538895a
MuellerPM <151080004+MuellerPM@users.noreply.github.com>
2023-11-30 22:19:11 -0600

    Lab Assignment 8
    Coding that created the multiple images of turkeys with a text description of "Tis the season to have some Turkey... Gobble Gobble Gobble".

--- sketch.js
+++ sketch.js
@@ -1,25 +1,17 @@
-var x1 = -10;
-var x2 = 10;
-var x3 = 35;
-var x4 = 18;
-var x5 = 30;
+var img1;
+var img2;
+
+function preload() {
+  img1 = loadImage("turkey.jpg.png");
+  img2 = loadImage("turkey_food.jpg");
+}
 
 function setup() {
   createCanvas(400, 400);
-  noStroke();
- }
+}
 
 function draw() {
-  background(0);
-  x1 += 0.7;
-  x2 += 0.7;
-  x3 += 0.7;
-  x4 += 0.7;
-  x5 += 0.7;
-  arc(x1, 20, 20, 20, 0.52, 5.76);
-  arc(x2, 40, 20, 20, 0.52, 5.76);
-  arc(x3, 60, 20, 20, 0.52, 5.76);
-  arc(x4, 80, 20, 20, 0.52, 5.76);
-  arc(x5, 100, 20, 20, 0.52, 5.76);
-  fill(350);
+  image(img1, -120, 0);
+  image(img1, 130, 0, 240, 120);
+  image(img2, 300, 0, 240, 120);
 }
