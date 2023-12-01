256c95f5af490e091fdd4d2f08d575fc78f189d0
MuellerPM <151080004+MuellerPM@users.noreply.github.com>
2023-11-30 22:16:28 -0600

    Lab Assignment 7
    Coding that create a lot of moving Pac-man.

--- sketch.js
+++ sketch.js
@@ -1,20 +1,25 @@
+var x1 = -10;
+var x2 = 10;
+var x3 = 35;
+var x4 = 18;
+var x5 = 30;
+
 function setup() {
   createCanvas(400, 400);
-}
+  noStroke();
+ }
 
 function draw() {
-  background(100);
-  translate(150, 350);
-  stroke(50);
-  strokeWeight(80);
-  line(0, -35, 0, -65); // Body
-  noStroke();
-  fill(400);
-  ellipse(-17.5, -65, 35, 35); // Left eye dome
-  ellipse(17.5, -65, 35, 35); // Right eye dome
-  arc(0, -65, 70, 70, 0, PI); // Chin
-  fill(10);
-  ellipse(-14, -65, 8, 8); // Left eye
-  ellipse(14, -65, 8, 8); // Right eye
-  quad(0, -58, 4, -51, 0, -44, -4, -51); // Beak
+  background(0);
+  x1 += 0.7;
+  x2 += 0.7;
+  x3 += 0.7;
+  x4 += 0.7;
+  x5 += 0.7;
+  arc(x1, 20, 20, 20, 0.52, 5.76);
+  arc(x2, 40, 20, 20, 0.52, 5.76);
+  arc(x3, 60, 20, 20, 0.52, 5.76);
+  arc(x4, 80, 20, 20, 0.52, 5.76);
+  arc(x5, 100, 20, 20, 0.52, 5.76);
+  fill(350);
 }
